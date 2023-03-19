5eba3e9acaf151a4f5ecd767447d32ebe1c9d8f6
hieuthai <hieuthai642@gmail.com>
2023-03-19 01:03:04 +0000

    chore: add test

--- src/test/java/org/example/InvoiceEmailerImplTest.java
+++ src/test/java/org/example/InvoiceEmailerImplTest.java
@@ -1,11 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 	@Test
-	void demoTest() {
+	@DisplayName("Test that email does include cancellation option when configured to do so")
+	void testIncludeOrderCancellation() {
+		FeatureDecisions featureDecisions = FeatureDecisionsFactory.fromInput(true);
+		InvoiceEmailerFactory invoiceEmailerFactory = FeatureAwareFactory_InvoiceEmailerFactory.createFeatureAwareFactoryBasedOn(featureDecisions);
+		InvoiceEmailer invoiceEmailer = invoiceEmailerFactory.createInvoiceEmailer();
+		Email email = invoiceEmailer.generateInvoiceEmail("test", "Hi", 10);
+
+		assertTrue(email.getContent().toLowerCase().contains("cancel"));
+	}
+
+	@Test
+	@DisplayName("Test that email does not include cancellation option when configured not to do so")
+	void testNotIncludeOrderCancellation() {
+		FeatureDecisions featureDecisions = FeatureDecisionsFactory.fromInput(false);
+		InvoiceEmailerFactory invoiceEmailerFactory = FeatureAwareFactory_InvoiceEmailerFactory.createFeatureAwareFactoryBasedOn(featureDecisions);
+		InvoiceEmailer invoiceEmailer = invoiceEmailerFactory.createInvoiceEmailer();
+		Email email = invoiceEmailer.generateInvoiceEmail("test", "Hi", 10);
+
+		assertFalse(email.getContent().toLowerCase().contains("cancel"));
 	}
 }
